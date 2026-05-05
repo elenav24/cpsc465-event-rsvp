2fb20cbc8aebd31cd4ad74e98d0842d6be815f08
Mark Garcia <contact@markgarcia.dev>
2026-05-04 21:32:50 -0700

    fix login

--- backend/services/users/app/deps/db.py
+++ backend/services/users/app/deps/db.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.core.config import DATABASE_URL
+from app.core.config import DATABASE_URL, DEBUG
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=DEBUG)
 SessionLocal = sessionmaker(engine, expire_on_commit=False)
 
 
